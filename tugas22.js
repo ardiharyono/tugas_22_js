397b2e97b6cb36db64a05fb677dcf1020341d1c4
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:37:18 +0700

    Tugas 22 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function panggilAku() {
+  var kata = ["Saya", "ingin", "belajar", "bersama"];
+  kata.forEach(function (item, index) {
+    console.log("Item : "+ item + " Index ke " +index);
+  })
+}
+
+console.log(panggilAku());
